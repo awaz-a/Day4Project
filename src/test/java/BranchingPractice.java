7992d9c39d90106c0ad23fda3711d9339969ab86
Awaz <awazfanwar@gmail.com>
2021-07-05 15:48:58 -0500

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,3 +1,12 @@
 public class BranchingPractice {
+    public static void main(String[] args) {
+
+
+        // created new project
+        //enable version control
+        //added.gitignore with .idea target and .iml
+        //made initial commit y keeping track of all files
+        //shared project
+    }
 
 }
